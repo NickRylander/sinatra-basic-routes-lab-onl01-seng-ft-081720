5fcff909e2697007073fe75d2f8ed262671fd2f3
NickRylander <nick.rylander@outlook.com>
2020-09-26 08:42:09 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Nick!"
+    end
+
+    get "/hometown" do
+        "My hometown is Sammamish, WA!"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is Dig Down by Muse!"
+    end
+
 end
